03b57ad6119737b093c1670cfdc8ad933ffbfa6b
Szymon Prud <UL0255098@edu.uni.lodz.pl>
2022-12-22 23:01:44 +0100

    new sword

--- TextRPG/src/Shop.h
+++ TextRPG/src/Shop.h
@@ -17,6 +17,7 @@ class Shop {
 		weapons.push_back(std::make_pair(Weapon("Dragon Sword", 5, 30, 30, 30), 20));
 		weapons.push_back(std::make_pair(Weapon("God Sword", 6, 35, 35, 35), 20));
 		weapons.push_back(std::make_pair(Weapon("Miecz Szymona", 7, 120, 95, 300), 250));
+		weapons.push_back(std::make_pair(Weapon("Harnas Sword", 8, 35, 35, 35), 20));
 
 		//armors.push_back(std::make_pair(Armor("Wooden Armor", 0, 5));
 		//armors.push_back(std::make_pair(Armor("Iron Armor", 1, 10));
